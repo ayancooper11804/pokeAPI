514c52b014ada5ca7ff56aa355381815442df097
student <student@MacBook-Pro.lan1>
2019-05-06 19:52:29 -0400

    New

--- pokemon-starter/scripts.js
+++ pokemon-starter/scripts.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
+	$("#btn1").click(getPokemon);
 
+	let pokemon = {
+		name: '',
+		attack: 0,
+		img: ''
+	};
+
+	function getPokemon(){
+		let id = Math.floor(Math.random()*800);
+		$.get(`https://pokeapi.co/api/v2/pokemon/${id}/`, function(response) {
+			console.log(response);
+			console.log(response.name);
+			$('#first').empty();
+			$('#first').append(`<h1>${response.name}</h1>`);
+
+		});
+	}
 })
